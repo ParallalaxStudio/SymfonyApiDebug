feecbdec6e2469b3bcb9b69694f403b767484001
Unknown <vivien.denizart@gmail.com>
2018-05-24 12:58:18 +0200

    profiler improvement
    log the params and the response in the profiler

--- DataCollector/RequestCollector.php
+++ DataCollector/RequestCollector.php
@@ -11,7 +11,6 @@ class RequestCollector extends DataCollector
 
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-
         $headers = $request->headers->all();
 
         if(array_key_exists('x-api-debug-data', $headers)) {
@@ -20,6 +19,12 @@ class RequestCollector extends DataCollector
             );
 
             $response->headers->set('x-api-debug-data', json_encode($request->headers->get('x-api-debug-data')));
+        }
+        
+        if(array_key_exists('x-api-response', $headers)) {
+
+            $this->data['api_response'] = $request->headers->get('x-api-response');
+            $request->headers->remove('x-api-response');
         }
     }
 
@@ -31,6 +36,10 @@ class RequestCollector extends DataCollector
     public function getApiDebugData()
     {
         return isset($this->data['api_debug_data']) ? $this->data['api_debug_data'] : null;
+    }
+
+    public function getResponse() {
+        return isset($this->data['api_response']) ? $this->data['api_response'] : null;
     }
 
     public function getName()
--- Services/RestClientAdapter.php
+++ Services/RestClientAdapter.php
@@ -5,28 +5,30 @@ namespace Parallalax\ApiDebugBundle\Services;
 use \Symfony\Component\HttpFoundation\RequestStack;
 
 use \GuzzleHttp\ClientInterface;
-use \GuzzleHttp\Psr7;
 use \Psr\Http\Message\RequestInterface;
 
 class RestClientAdapter implements ClientInterface{
 
+    protected $env;//to catch the ws requests only in the dev environment
     protected $request;
-    protected $client;
-    protected $auth;
+    protected $client;//the restClient
     protected $uri;
+    protected $sentData;
     protected $executionTime;
 
     protected $response;
 
-    public function __construct(RequestStack $request) {
+    public function __construct(RequestStack $request, $env) {
         $this->request = $request->getCurrentRequest();
         $this->client = new \GuzzleHttp\Client();
+        $this->env = $env;
     }
 
 
     public function send(RequestInterface $request, array $options = []) {
 
         $this->uri = $request->getUri();
+        $this->sentData = $options;
         $executionStartTime = microtime(true);
         $this->response = $this->client->send($request, $options);
         $executionEndTime = microtime(true);
@@ -40,6 +42,7 @@ class RestClientAdapter implements ClientInterface{
     public function sendAsync(RequestInterface $request, array $options = [])
     {
         $this->uri = $request->getUri();
+        $this->sentData = $options;
         $executionStartTime = microtime(true);
         $this->response = $this->client->sendAsync($request, $options);
         $executionEndTime = microtime(true);
@@ -53,6 +56,7 @@ class RestClientAdapter implements ClientInterface{
     public function request($method, $uri, array $options = []) {
 
         $this->uri = $uri;
+        $this->sentData = $options;
         $executionStartTime = microtime(true);
         $this->response = $this->client->request($method, $uri, $options);
         $executionEndTime = microtime(true);
@@ -66,6 +70,7 @@ class RestClientAdapter implements ClientInterface{
     public function requestAsync($method, $uri, array $options = []) {
 
         $this->uri = $uri;
+        $this->sentData = $options;
         $executionStartTime = microtime(true);
         $this->response = $this->client->requestAsync($method, $uri, $options);
         $executionEndTime = microtime(true);
@@ -80,45 +85,54 @@ class RestClientAdapter implements ClientInterface{
         return $this->client->getConfig($option);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /**
+     * modify the request with the api calls params to get catched by the dataCollector
+     * @param  string $method
+     * @return void
+     */
     private function modifyRequestHeaders($method) {
 
-        $headers = $this->response->getHeaders();
+        if($this->env == 'dev') {//only useful in dev env
+            $headers = $this->response->getHeaders();
 
-        $data = array();
-        $xDebugdata = array();
+            $data = array();
+            $dataResponse = array();
+            $xDebugdata = array();
+            $xDebugResponse = array();
 
-        if($this->request->headers->has('X-API-Debug-Data')) {
-            $xDebugdata = $this->request->headers->get('X-API-Debug-Data');
+            if($this->request->headers->has('X-API-Debug-Data')) {
+                $xDebugdata = $this->request->headers->get('X-API-Debug-Data');
+            }
+
+            if($this->request->headers->has('X-API-Response')) {
+                $xDebugResponse = $this->request->headers->get('X-API-Response');
+            }
+
+            if(array_key_exists('X-Debug-Token', $headers)) {
+                $data = ['token' => $this->response->getHeaderLine('X-Debug-Token'), 'profiler' => $this->response->getHeaderLine('X-Debug-Token-Link')];
+            }
+
+            $data['method'] = $method;
+            $data['status'] = $this->response->getStatusCode();
+            $data['uri'] = $this->uri;
+            $data['time'] = $this->executionTime;
+
+            $xDebugdata[] = $data;
+
+            $dataResponse['sentData'] = $this->sentData;
+
+            //if json : decode the response, else get html length
+            if(strpos($this->response->getHeaderLine('content-type'), 'application/json') !== false) {
+                $dataResponse['response'] = ['type' => 'json', 'content' => json_decode($this->response->getBody())];
+            }
+            else {
+                $dataResponse['response'] = ['type' => 'html', 'content' =>  strlen($this->response->getBody()) .'o'];
+            }
+
+            $xDebugResponse[] = $dataResponse;
+
+            $this->request->headers->set('X-API-Debug-Data', [$xDebugdata]);
+            $this->request->headers->set('X-API-Response', [$xDebugResponse]);
         }
-
-        if(array_key_exists('X-Debug-Token', $headers)) {
-
-            $data = ['token' => $this->response->getHeaderLine('X-Debug-Token'), 'profiler' => $this->response->getHeaderLine('X-Debug-Token-Link')];
-        }
-
-        $data['method'] = $method;
-        $data['status'] = $this->response->getStatusCode();
-        $data['url'] = $this->uri;
-        $data['time'] = $this->executionTime;
-
-        $xDebugdata[] = $data;
-
-        $this->request->headers->set('X-API-Debug-Data', [$xDebugdata]);
     }
 }
